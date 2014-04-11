57f78f13b5bc13ccf6a63cb8631e3e57050ebb87
mschneider90 <michael.schneider90@gmail.com>
2014-04-11 14:36:06 -0700

    makefiles

--- Tri-copter/_build/L5_Application/source/subdir.mk
+++ Tri-copter/_build/L5_Application/source/subdir.mk
@@ -4,6 +4,7 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
+../L5_Application/source/10dof.cpp \
 ../L5_Application/source/ServoController.cpp \
 ../L5_Application/source/high_level_init.cpp \
 ../L5_Application/source/remote.cpp \
@@ -14,6 +15,7 @@ C_SRCS += \
 ../L5_Application/source/LCD.c 
 
 OBJS += \
+./L5_Application/source/10dof.o \
 ./L5_Application/source/BM_Controller.o \
 ./L5_Application/source/LCD.o \
 ./L5_Application/source/ServoController.o \
@@ -26,6 +28,7 @@ C_DEPS += \
 ./L5_Application/source/LCD.d 
 
 CPP_DEPS += \
+./L5_Application/source/10dof.d \
 ./L5_Application/source/ServoController.d \
 ./L5_Application/source/high_level_init.d \
 ./L5_Application/source/remote.d \
@@ -33,17 +36,17 @@ CPP_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-L5_Application/source/%.o: ../L5_Application/source/%.c
-	@echo 'Building file: $<'
-	@echo 'Invoking: Cross ARM C Compiler'
-	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wall -I"C:\Users\sloretz\git\Tricopter\Tri-copter" -I"C:\Users\sloretz\git\Tricopter\Tri-copter\newlib" -I"C:\Users\sloretz\git\Tricopter\Tri-copter\L0_LowLevel" -I"C:\Users\sloretz\git\Tricopter\Tri-copter\L1_FreeRTOS" -I"C:\Users\sloretz\git\Tricopter\Tri-copter\L1_FreeRTOS\include" -I"C:\Users\sloretz\git\Tricopter\Tri-copter\L1_FreeRTOS\portable" -I"C:\Users\sloretz\git\Tricopter\Tri-copter\L2_Drivers" -I"C:\Users\sloretz\git\Tricopter\Tri-copter\L2_Drivers\base" -I"C:\Users\sloretz\git\Tricopter\Tri-copter\L3_Utils" -I"C:\Users\sloretz\git\Tricopter\Tri-copter\L3_Utils\tlm" -I"C:\Users\sloretz\git\Tricopter\Tri-copter\L4_IO" -I"C:\Users\sloretz\git\Tricopter\Tri-copter\L4_IO\fat" -I"C:\Users\sloretz\git\Tricopter\Tri-copter\L4_IO\wireless" -I"C:\Users\sloretz\git\Tricopter\Tri-copter\L5_Application" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
-	@echo 'Finished building: $<'
-	@echo ' '
-
 L5_Application/source/%.o: ../L5_Application/source/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross ARM C++ Compiler'
-	arm-none-eabi-g++ -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wall -I"C:\Users\sloretz\git\Tricopter\Tri-copter" -I"C:\Users\sloretz\git\Tricopter\Tri-copter\newlib" -I"C:\Users\sloretz\git\Tricopter\Tri-copter\L0_LowLevel" -I"C:\Users\sloretz\git\Tricopter\Tri-copter\L1_FreeRTOS" -I"C:\Users\sloretz\git\Tricopter\Tri-copter\L1_FreeRTOS\include" -I"C:\Users\sloretz\git\Tricopter\Tri-copter\L1_FreeRTOS\portable" -I"C:\Users\sloretz\git\Tricopter\Tri-copter\L2_Drivers" -I"C:\Users\sloretz\git\Tricopter\Tri-copter\L2_Drivers\base" -I"C:\Users\sloretz\git\Tricopter\Tri-copter\L3_Utils" -I"C:\Users\sloretz\git\Tricopter\Tri-copter\L3_Utils\tlm" -I"C:\Users\sloretz\git\Tricopter\Tri-copter\L4_IO" -I"C:\Users\sloretz\git\Tricopter\Tri-copter\L4_IO\fat" -I"C:\Users\sloretz\git\Tricopter\Tri-copter\L4_IO\wireless" -I"C:\Users\sloretz\git\Tricopter\Tri-copter\L5_Application" -fno-exceptions -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
+	arm-none-eabi-g++ -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wall -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter" -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\newlib" -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\L0_LowLevel" -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\L1_FreeRTOS" -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\L1_FreeRTOS\include" -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\L1_FreeRTOS\portable" -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\L2_Drivers" -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\L2_Drivers\base" -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\L3_Utils" -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\L3_Utils\tlm" -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\L4_IO" -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\L4_IO\fat" -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\L4_IO\wireless" -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\L5_Application" -fno-exceptions -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+L5_Application/source/%.o: ../L5_Application/source/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wall -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter" -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\newlib" -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\L0_LowLevel" -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\L1_FreeRTOS" -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\L1_FreeRTOS\include" -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\L1_FreeRTOS\portable" -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\L2_Drivers" -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\L2_Drivers\base" -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\L3_Utils" -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\L3_Utils\tlm" -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\L4_IO" -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\L4_IO\fat" -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\L4_IO\wireless" -I"C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\L5_Application" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- Tri-copter/_build/newlib/newlib_time.d
+++ Tri-copter/_build/newlib/newlib_time.d
@@ -1,4 +1,4 @@
 newlib/newlib_time.d: ../newlib/newlib_time.c \
- C:\Users\sloretz\git\Tricopter\Tri-copter\L2_Drivers/rtc.h
+ C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\L2_Drivers/rtc.h
 
-C:\Users\sloretz\git\Tricopter\Tri-copter\L2_Drivers/rtc.h:
+C:\Users\Michael\Documents\GitHub\Tricopter\Tri-copter\L2_Drivers/rtc.h:
